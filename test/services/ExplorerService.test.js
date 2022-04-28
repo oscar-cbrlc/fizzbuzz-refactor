ca322499dce6611b97b66366cfc27ce778bec3f0
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-27 23:07:37 -0500

    Updated description for ExplorerService filterByMission test group

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,7 +4,7 @@ const ExplorerService = require('./../../lib/services/ExplorerService')
 // tested in Reader.test.js
 const explorers = Reader.readJsonFile('./../../test/info/explorers.json')
 
-describe('Explorer Service method tests', () => {
+describe('Explorer Service filterByMission tests', () => {
     test('should return a not-undefined list of explorers filtered by a mission', () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         expect(explorersInNode).not.toBeUndefined()
