4f508b3aa6c23bbf6cd4597b37645e4953fd6c53
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-27 21:20:58 -0500

    Added unit test for Reader readJsonFile method

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,8 @@
 const Reader = require('./../../lib/utils.Reader')
-
+const jsonFilePath = './../info/explorers.json'
 describe('Read class testing', () => {
-    test('should ', () => {
-        
+    test('should return a parsed js object from json file', () => {
+        const parsedObject = Reader.readJsonFile(jsonFilePath)
+        expect(parsedObject).not.toBeUndefined()
     });
 });
