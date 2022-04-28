5772a4e709de0b3ca8d0a053264eefe476f8b32f
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-27 21:28:51 -0500

    Fixed Reader class file path in Reader test file

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,4 +1,4 @@
-const Reader = require('./../../lib/utils.Reader')
+const Reader = require('./../../lib/utils/Reader')
 const jsonFilePath = './../info/explorers.json'
 describe('Read class testing', () => {
     test('should return a parsed js object from json file', () => {
