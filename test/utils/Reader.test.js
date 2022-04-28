718390b94b0cc65e4c9898e3be5fd845719e65e9
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-27 22:31:54 -0500

    Fixed file path finding

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,5 +1,5 @@
 const Reader = require('./../../lib/utils/Reader')
-const jsonFilePath = './../info/explorers.json'
+const jsonFilePath = './../../test/info/explorers.json'
 describe('Read class testing', () => {
     test('should return a parsed js object from json file', () => {
         const parsedObject = Reader.readJsonFile(jsonFilePath)
