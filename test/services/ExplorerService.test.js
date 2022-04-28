8db29d36e28ad22e33ffd75671c77cb781a6cfbf
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-27 23:16:15 -0500

    Added a ExplorerService test for invalid values in second method

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,12 +6,19 @@ const explorers = Reader.readJsonFile('./../../test/info/explorers.json')
 
 describe('Explorer Service filterByMission tests', () => {
     test('should return a not-undefined list of explorers filtered by a mission', () => {
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
+        const explorersInNode = ExplorerService.filterByMission(explorers, "ad")
         expect(explorersInNode).not.toBeUndefined()
     });
 
     test('should return a not-empty list of explorers given an existing mission (in any of them)', () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         expect(explorersInNode.length).toBeGreaterThan(0)
+    });
+});
+
+describe('Explorer Service getAmountOfExplorersByMission tests', () => {
+    test('should get 0, given an invalid value', () => {
+        const numberOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, 321)
+        expect(numberOfExplorers).toEqual(0)
     });
 });
