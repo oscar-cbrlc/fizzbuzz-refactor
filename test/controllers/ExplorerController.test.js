539a843c6599d94987cc8cf335ef9256739e6854
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-29 16:11:48 -0500

    Added test for first ExplorerController's method

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,4 +1,14 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
-describe('Tests for Explorer Controller', () => {
-    
+const ExplorerService = require("../../lib/services/ExplorerService");
+const Reader = require("./../../lib/utils/reader")
+const explorers = Reader.readJsonFile("./../explorers.json")
+
+describe("Tests for Explorer Controller", () => {
+    test("should get the correct list, given any value", () => {
+        // this is already tested in the service, so we get the values, 
+        // and check in the controller method if they're the same
+        const explorersFromService = ExplorerService.filterByMission(explorers, "node")
+        const explorersFromController = ExplorerController.getExplorersByMission("node")
+        expect(explorersFromController).toEqual(explorersFromService)
+    });
 });
