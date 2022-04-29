30ab8ff5a1f42bf852707a94a1615d07bbf280df
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-29 18:59:30 -0500

    Fixed code style to match linter rules

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,26 +1,26 @@
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 const ExplorerService = require("../../lib/services/ExplorerService");
-const Reader = require("./../../lib/utils/reader")
-const explorers = Reader.readJsonFile("./../explorers.json")
+const Reader = require("./../../lib/utils/reader");
+const explorers = Reader.readJsonFile("./../explorers.json");
 
 describe("Tests for Explorer Controller", () => {
     test("[getExplorersByMission] should get the correct list, given any value", () => {
         // this is already tested in the service, so we get the values, 
         // and check in the controller method if they're the same
-        const explorersFromService = ExplorerService.filterByMission(explorers, "node")
-        const explorersFromController = ExplorerController.getExplorersByMission("node")
-        expect(explorersFromController).toEqual(explorersFromService)
+        const explorersFromService = ExplorerService.filterByMission(explorers, "node");
+        const explorersFromController = ExplorerController.getExplorersByMission("node");
+        expect(explorersFromController).toEqual(explorersFromService);
     });
 
     test("[getExplorersUsernamesByMission] should get the correct list, given any value", () => {
-        const usernamesFromService = ExplorerService.getExplorersUsernamesByMission(explorers, "java")
-        const usernamesFromController = ExplorerController.getExplorersUsernamesByMission("java")
-        expect(usernamesFromController).toEqual(usernamesFromService)
+        const usernamesFromService = ExplorerService.getExplorersUsernamesByMission(explorers, "java");
+        const usernamesFromController = ExplorerController.getExplorersUsernamesByMission("java");
+        expect(usernamesFromController).toEqual(usernamesFromService);
     });
     
     test("[getExplorersAmonutByMission] should get the correct number, given any value", () => {
-        const numberFromService = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
-        const numberFromController = ExplorerController.getAmountOfExplorersByMission("node")
-        expect(numberFromService).toEqual(numberFromController)
+        const numberFromService = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        const numberFromController = ExplorerController.getAmountOfExplorersByMission("node");
+        expect(numberFromService).toEqual(numberFromController);
     });
 });
