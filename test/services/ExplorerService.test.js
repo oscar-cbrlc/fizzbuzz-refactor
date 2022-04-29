3967d6dbda69ec5350e783be09cbf0d73c9514c2
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-28 22:36:04 -0500

    Fixed files to match set Linter rules

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,42 +1,42 @@
-const Reader = require('./../../lib/utils/Reader')
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const Reader = require("./../../lib/utils/Reader");
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 // tested in Reader.test.js
-const explorers = Reader.readJsonFile('./../../test/info/explorers.json')
+const explorers = Reader.readJsonFile("./../../test/info/explorers.json");
 
 // ... //
 
-describe('Explorer Service filterByMission tests', () => {
-    test('should return a not-undefined list of explorers filtered by a mission', () => {
-        const explorersInNode = ExplorerService.filterByMission(explorers, "ad")
-        expect(explorersInNode).not.toBeUndefined()
+describe("Explorer Service filterByMission tests", () => {
+    test("should return a not-undefined list of explorers filtered by a mission", () => {
+        const explorersInNode = ExplorerService.filterByMission(explorers, "ad");
+        expect(explorersInNode).not.toBeUndefined();
     });
 
-    test('should return a not-empty list of explorers given an existing mission (in any of them)', () => {
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBeGreaterThan(0)
+    test("should return a not-empty list of explorers given an existing mission (in any of them)", () => {
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode.length).toBeGreaterThan(0);
     });
 });
 
-describe('Explorer Service getAmountOfExplorersByMission tests', () => {
-    test('should get 0, given an invalid value', () => {
+describe("Explorer Service getAmountOfExplorersByMission tests", () => {
+    test("should get 0, given an invalid value", () => {
         // also works for non-existing values
-        const numberOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, 321)
-        expect(numberOfExplorers).toEqual(0)
+        const numberOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, 321);
+        expect(numberOfExplorers).toEqual(0);
     });
-    test('should get more than 0, given an existing value', () => {
-        const numberOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "java")
-        expect(numberOfExplorers).toBeGreaterThan(0)
+    test("should get more than 0, given an existing value", () => {
+        const numberOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "java");
+        expect(numberOfExplorers).toBeGreaterThan(0);
     });
 });
 
-describe('ExplorerService getExplorersUsernamesByMission tests', () => {
-    test('should get an empty array if given wrong value', () => {
-        const usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "C#")
-        expect(usernames.length).toEqual(0)
+describe("ExplorerService getExplorersUsernamesByMission tests", () => {
+    test("should get an empty array if given wrong value", () => {
+        const usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "C#");
+        expect(usernames.length).toEqual(0);
     });
-    test('should get a username list if given existing value', () => {
-        const usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "css")
-        expect(usernames).toEqual(['ajolonauta16', 'ajolonauta17'])
+    test("should get a username list if given existing value", () => {
+        const usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "css");
+        expect(usernames).toEqual(["ajolonauta16", "ajolonauta17"]);
     });
 });
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,8 +1,8 @@
-const Reader = require('./../../lib/utils/Reader')
-const jsonFilePath = './../../test/info/explorers.json'
-describe('Read class testing', () => {
-    test('should return a parsed js object from json file', () => {
-        const parsedObject = Reader.readJsonFile(jsonFilePath)
-        expect(parsedObject).not.toBeUndefined()
+const Reader = require("./../../lib/utils/Reader");
+const jsonFilePath = "./../../test/info/explorers.json";
+describe("Read class testing", () => {
+    test("should return a parsed js object from json file", () => {
+        const parsedObject = Reader.readJsonFile(jsonFilePath);
+        expect(parsedObject).not.toBeUndefined();
     });
 });
