4ecc661e019424ee1f1575376bf0225c77c970fd
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-29 19:47:54 -0500

    Added tests for Fizzbuzz validation number

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,6 +1,6 @@
 const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
-describe("Tests for FizzBuzzService", () => {
+describe("Tests for [applyValidationInExplorer]", () => {
     test("should set \"trick\" as score value", () => {
         const explorer = {name: "Explorer", score: 1};
         FizzbuzzService.applyValidationInExplorer(explorer);
@@ -20,5 +20,25 @@ describe("Tests for FizzBuzzService", () => {
         const explorer = {name: "Explorer", score: 15};
         FizzbuzzService.applyValidationInExplorer(explorer);
         expect(explorer.trick).toEqual("FIZZBUZZ");
+    });
+});
+
+
+describe("Tests for [applyValidationInNumber]", () => {
+    test("should set \"trick\" as score value", () => {
+        const trick = FizzbuzzService.applyValidationInNumber(1);
+        expect(trick).toEqual(1);
+    });
+    test("should set \"trick\" as \"FIZZ\"", () => {
+        const trick = FizzbuzzService.applyValidationInNumber(3);
+        expect(trick).toEqual("FIZZ");
+    });
+    test("should set \"trick\" as \"BUZZ\"", () => {
+        const trick = FizzbuzzService.applyValidationInNumber(5);
+        expect(trick).toEqual("BUZZ");
+    });
+    test("should set \"trick\" as \"FIZZBUZZ\"", () => {
+        const trick = FizzbuzzService.applyValidationInNumber(15);
+        expect(trick).toEqual("FIZZBUZZ");
     });
 });
